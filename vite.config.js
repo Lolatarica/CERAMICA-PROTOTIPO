09a9fe0ac8a54db359c0ac64bf7269ab73466100
Lolatarica <lolatarica@gmail.com>
2026-04-17 01:45:10 -0300

    actualizacion de vite config y build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/APP-CERAMICA-PROTOTIPO/', // Reemplaza esto con el nombre exacto de tu repositorio
+  base: '/CERAMICA-PROTOTIPO/', // Reemplaza esto con el nombre exacto de tu repositorio
   })
